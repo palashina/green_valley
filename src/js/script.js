1914d975d07f5950deb655f440875fbc74d5a614
palashina <palashina@gmail.com>
2016-12-20 23:13:52 +0300

    закомитила прописанное owl в script.js

--- src/js/script.js
+++ src/js/script.js
@@ -2,29 +2,29 @@ $(document).ready( function() {
 
 /*галерея на главной*/
 
-$('#gallery-thumbs').owlCarousel({
-  items:3,
-  loop:true,
-  center:true,
-  responsive: {
-    768: {
-      items:4,
-    },
-    1200: {
-      items:5,
-    },
-  },
-});
+// $('#gallery-thumbs').owlCarousel({
+//   items:3,
+//   loop:true,
+//   center:true,
+//   responsive: {
+//     768: {
+//       items:4,
+//     },
+//     1200: {
+//       items:5,
+//     },
+//   },
+// });
 
-var mainImgUrl = $('#gallery-start-photo').attr('href');
+// var mainImgUrl = $('#gallery-start-photo').attr('href');
 
-$('#gallery-main-pict').html('<img src="'+mainImgUrl+'" alt="" />');
+// $('#gallery-main-pict').html('<img src="'+mainImgUrl+'" alt="" />');
 
-$('#gallery-thumbs a').on('click', function(e){
-  e.preventDefault();
-  var imageUrl = $(this).attr('href');
-  $('#gallery-main-pict').html('<img src="'+imageUrl+'" alt="" />');
-});
+// $('#gallery-thumbs a').on('click', function(e){
+//   e.preventDefault();
+//   var imageUrl = $(this).attr('href');
+//   $('#gallery-main-pict').html('<img src="'+imageUrl+'" alt="" />');
+// });
 
 /*табы, переключалка на главной*/
 
